11a00746303799eb0288797d201ffd93886537c7
Sitaye <1091833485@qq.com>
2024-11-17 17:16:07 +0800

    initial commit

---
+++ lua/plugins/colorcheme.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "HiPhish/rainbow-delimiters.nvim"
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {},
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-mocha",
+        }
+    },
+}
